2f7ab5b73e0b9cc2ac330c966fdcdd8f2a672031
Anurag Yadav <147136323+Lucifer-AY@users.noreply.github.com>
2025-04-04 14:10:56 +0530

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# HR-Agent
-The project is able to parse resumes, rank the and then schedule meeting along with automated email sending. It can also conduct interviews.
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-def setup_logging():
-    """Setup logging configuration."""
-    logging.basicConfig(filename='data/logs/app.log', level=logging.INFO, 
-                        format='%(asctime)s - %(levelname)s - %(message)s')
